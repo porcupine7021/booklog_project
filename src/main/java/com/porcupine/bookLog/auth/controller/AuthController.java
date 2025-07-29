3fd71da58a3684945b4995749855ea34d688494f
yeone <lovewl1021@naver.com>
2025-07-30 00:00:19 +0900

    --AuthController Test

---
+++ src/main/java/com/porcupine/bookLog/auth/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.porcupine.bookLog.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+}
